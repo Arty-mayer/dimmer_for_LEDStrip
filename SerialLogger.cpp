d63583a8ecf515c5a1d4a755c227f670cfb74f11
Artem <mayer.arty@gmail.com>
2026-02-20 18:47:26 +0100

    v1.1 b

--- SerialLogger.cpp
+++ SerialLogger.cpp
@@ -2,25 +2,98 @@
 
 bool Logger::enabled = false; // ОБЯЗАТЕЛЬНО вне класса!
 
-void Logger::log(const char *message, bool newLine = true)
+void Logger::log(const char *module, const char *message)
 {
-    if (Logger::enabled)
+    if (printPrefix(module, message))
     {
-        if (newLine)
-        {
-            Serial.println();
-        }
-        else
-        {
-            Serial.print(message);
-        }
+        Serial.println();
     }
+}
+
+void Logger::log(const char *module, const char *message, uint8_t value)
+{
+    if (printPrefix(module, message))
+    {
+        printSeparator();
+        Serial.println(value);
+    }
+}
+
+void Logger::log(const char *module, const char *message, int8_t value)
+{
+    if (printPrefix(module, message))
+    {
+        printSeparator();
+        Serial.println(value);
+    }
+}
+
+void Logger::log(const char *module, const char *message, uint16_t value)
+{
+    if (printPrefix(module, message))
+    {
+        printSeparator();
+        Serial.println(value);
+    }
+}
+
+void Logger::log(const char *module, const char *message, int16_t value)
+{
+    if (printPrefix(module, message))
+    {
+        printSeparator();
+        Serial.println(value);
+    }
+}
+
+void Logger::log(const char *module, const char *message, uint32_t value)
+{
+    if (printPrefix(module, message))
+    {
+        printSeparator();
+        Serial.println(value);
+    }
+}
+
+void Logger::log(const char *module, const char *message, int32_t value)
+{
+    if (printPrefix(module, message))
+    {
+        printSeparator();
+        Serial.println(value);
+    }
+}
+
+void Logger::log(const char *module, const char *message, const char *value)
+{
+    if (printPrefix(module, message))
+    {
+        printSeparator();
+        Serial.println(value);
+    }
+}
+
+bool Logger::printPrefix(const char *module, const char *message)
+{
+    if (!enabled)
+        return false;
+
+    Serial.print(module);
+    Serial.print(F(" :> "));
+    Serial.print(message);
+    return true;
+}
+
+void Logger::printSeparator()
+{
+    Serial.print(F(" -> "));
 }
 
 void Logger::enableLogging(uint32_t speed)
 {
+    if (enabled) return;
     enabled = true;
     Serial.begin(speed); // Инициализация Serial при включении логирования
-    Serial.println("Logging activated");
-    Serial.println("Start Setup");
+    Serial.println(F("Logging activated"));
+    Serial.println(F("Start Setup"));
 }
--- SerialLogger.h
+++ SerialLogger.h
@@ -3,15 +3,27 @@
 
 #include <Arduino.h>
 
+#define LOG(module, msg) Logger::log(module, msg)
+#define LOGV(module, msg, value) Logger::log(module, msg, value)
+
 class Logger
 {
 public:
-    static bool enabled; // Флаг для включения/выключения логирования
-
-    static void log(const char *message, bool newLine = true);
+    static void log(const char *module, const char *message);
+    static void log(const char *module, const char *message, uint8_t value);
+    static void log(const char *module, const char *message, int8_t value);
+    static void log(const char *module, const char *message, uint16_t value);
+    static void log(const char *module, const char *message, int16_t value);
+    static void log(const char *module, const char *message, uint32_t value);
+    static void log(const char *module, const char *message, int32_t value);
+    static void log(const char *module, const char *message, const char *value);
 
     static void enableLogging(uint32_t speed = 115200);
-   
+
+private:
+    static bool enabled; // Флаг для включения/выключения логирования
+    static bool printPrefix(const char *module, const char *message);
+    static void printSeparator();
 };
 
 #endif
